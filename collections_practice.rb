84697f742ef35b408e49ec3e23eafcf0fe30804e
Eric J Wilhite <ericjwilhite@gmail.com>
2019-07-16 15:54:01 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -5,6 +5,13 @@ def sort_array_asc(array)
 end
 
 def sort_array_asc(array)
-  array.sort 
-  
+  array.sort do |a, b|
+    if a == b
+      0
+    elsif a < b
+      1
+    else
+      -1
+    end
+  end
 end
