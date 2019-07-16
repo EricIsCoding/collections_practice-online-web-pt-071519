9fb73243a074677b7395467c79b558a2e5a8fb8a
Eric J Wilhite <ericjwilhite@gmail.com>
2019-07-16 16:16:55 +0000

    Automatically backed up by Learn

--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -32,7 +32,7 @@ describe 'collections practice' do
   
   # Question 4.5
   describe '#swap_elements_from_to' do
-    it 'swap the second and third elements of an array' do
+    it 'swap one element of an array with another element in the array' do
       expect(swap_elements_from_to(["a", "b", "c"],0,2)).to eq(["c", "b", "a"])
     end
   end
